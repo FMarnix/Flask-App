b9121e79c87d6ed6f69b648d963bd589b4ee9d86
Filipe Neto <47115824+FMarnix@users.noreply.github.com>
2026-02-04 16:45:07 +0000

    chapter 3 files

--- config.py
+++ config.py
@@ -5,5 +5,6 @@ class ProdConfig(Config):
     pass
 
 class DevConfig(Config):
+    SECRET_KEY = "\r\xf2\x08\xe5RC\xd1\xb9\x8f)=6\x91\x0c\xcb\xc9\xa6\x9dt\xe1\x02o\x9a\xb6"
     debug = True
     SQLALCHEMY_DATABASE_URI = "sqlite:///dev.db"
